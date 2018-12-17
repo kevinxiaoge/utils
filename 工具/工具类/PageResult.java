5bc6416bd38a71e53a385d5550c980eafc478fe2
yuanyuan <yuanyuan@itheima.com>
2018-12-17 18:04:49 +0800

    的

---
+++ 工具/工具类/PageResult.java
@@ -0,0 +1,37 @@
+package entity;
+
+import java.util.List;
+
+/**
+ * @author ljh
+ * @version 1.0
+ * @description 描述
+ * @title 标题
+ * @package entity
+ * @company www.itheima.com
+ */
+public class PageResult {
+    private long total;//总记录数
+    private List rows;//当前页结果
+    public PageResult(long total, List rows) {
+        super();
+        this.total = total;
+        this.rows = rows;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List getRows() {
+        return rows;
+    }
+
+    public void setRows(List rows) {
+        this.rows = rows;
+    }
+}
